408893f9b400dd5b24deaec075690156c0159e92
carloveronesi <veronesicarlo@outlook.it>
2017-11-10 16:41:47 +0100

    correzione nome costanti

--- js/game.js
+++ js/game.js
@@ -1,20 +1,20 @@
 //SETTINGS
-const canvasWidth = 360;
-const canvasHeight = 360;
-const appleColor = "red";
-const snakeColor = "green";
-const strokeColor = "black";
-const squareDim = 19;
-const snakeInitLen = 3;
-const maxScore = 3;
-const updateInterval = 200;
-const gameOverMessage = "GAME OVER";
-const winMessage = "VITTORIA!";
-const messageFontDim = 30;
-const font = "Georgia";
-const messageColor = "blue";
-const scoreFontDim = 15;
-const snakeDirection = {x: 1, y:0};
+const CANVASWIDTH = 360;
+const CANVASHEIGHT = 360;
+const APPLECOLOR = "red";
+const SNAKECOLOR = "green";
+const STROKECOLOR = "black";
+const SQUAREDIM = 19;
+const SNAKEINITLEN = 3;
+const MAXSCORE = 3;
+const UPDATEINTERVAL = 200;
+const GAMEOVERMESSAGE = "GAME OVER";
+const WINMESSAGE = "VITTORIA!";
+const MESSAGEFONTDIM = 30;
+const FONT = "Georgia";
+const MESSAGECOLOR = "blue";
+const SCOREFONTDIM = 15;
+const SNAKEDIRECTION = {x: 1, y:0};
 
 var Apple, Snake, Score;
 
@@ -31,12 +31,12 @@ function startGame() {																						//INIZIALIZZO il gioco
 var GameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
-        this.canvas.width = canvasWidth;																	//Setto lunghezza area gioco
-		this.canvas.height = canvasHeight;																	//Setto altezza area gioco
+        this.canvas.width = CANVASWIDTH;																	//Setto lunghezza area gioco
+		this.canvas.height = CANVASHEIGHT;																	//Setto altezza area gioco
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);								//Inserisco prima dei comandi
 		
-        this.interval = setInterval(updateGameArea, updateInterval);										//Setto intervallo aggiornamento (in ms)
+        this.interval = setInterval(updateGameArea, UPDATEINTERVAL);										//Setto intervallo aggiornamento (in ms)
 	},
 	clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);								//Pulisco
@@ -50,7 +50,7 @@ function printSquare(x, y, width, color) {
 	ctx.rect(x*width, y*width, width, width);																//Stampo il rettangolo moltiplicando posizione per dimensione rettangolo
 	ctx.fillStyle = color;																					//Colore riempimento
 	ctx.fill();
-	ctx.strokeStyle = strokeColor;																			//Colore Contorno
+	ctx.strokeStyle = STROKECOLOR;																			//Colore Contorno
 	ctx.stroke();	
 	ctx.closePath();																						//FINE FORMA
 }
@@ -58,11 +58,11 @@ function printSquare(x, y, width, color) {
 //Snake
 function Snake(){
 	//Settaggi vari
-	this.width = squareDim;
-	this.color = snakeColor;
-	this.initLength = snakeInitLen;
+	this.width = SQUAREDIM;
+	this.color = SNAKECOLOR;
+	this.initLength = SNAKEINITLEN;
 	this.body = [];																							//Vettore contenente le posizioni dei rettangoli
-	this.direction = snakeDirection;																		//Setto la direzione iniziale
+	this.direction = SNAKEDIRECTION;																		//Setto la direzione iniziale
 
 
 	this.init = function(){																					//Inizializzo il vettore posizionando in alto a sx
@@ -87,9 +87,9 @@ function Snake(){
 		eatApple(SnakeX, SnakeY);																			//Controllo se la mela è mangiata
 
 		//Eseguo controllo (uscita dall'area consentita o collisione con il corpo)
-		if(SnakeX == canvasWidth/(Snake.width+1)+1 || SnakeX == -1 || SnakeY == canvasHeight/(Snake.width+1)+1 || SnakeY == -1 ){
+		if(SnakeX == CANVASWIDTH/(Snake.width+1)+1 || SnakeX == -1 || SnakeY == CANVASHEIGHT/(Snake.width+1)+1 || SnakeY == -1 ){
 			over = true;		
-			message(gameOverMessage);																		//GAME OVER
+			message(GAMEOVERMESSAGE);																		//GAME OVER
 		}else{
 			tail.x = SnakeX; 																				//Ok, setto la nuova posizione
 			tail.y = SnakeY;
@@ -131,16 +131,16 @@ function sposta(direzione){
 //Mela
 function Apple() {
 	//impostazioni varie
-    this.width = squareDim;
-	this.color = appleColor;
+    this.width = SQUAREDIM;
+	this.color = APPLECOLOR;
     this.x;
     this.y;    
     this.update = function() {																				//Stampo la mela
 		printSquare(this.x, this.y, this.width, this.color);
 	}
     this.newPos = function() {																				//Cambio posizione
-		this.x = Math.floor(Math.random()*(canvasWidth/(this.width+1)));
-		this.y = Math.floor(Math.random()*(canvasWidth/(this.width+1)));     
+		this.x = Math.floor(Math.random()*(CANVASWIDTH/(this.width+1)));
+		this.y = Math.floor(Math.random()*(CANVASWIDTH/(this.width+1)));     
 	}    
 }
 
@@ -160,24 +160,24 @@ function eatApple(x, y){
 function message(testo){
 	clearInterval(GameArea.interval);																		//Blocco aggiornamento frame
 	GameArea.clear();																						//Pulisco schermo
-	ctx.fillStyle = messageColor;
-	ctx.font= messageFontDim +"px " + font;
+	ctx.fillStyle = MESSAGECOLOR;
+	ctx.FONT= MESSAGEFONTDIM +"px " + FONT;
 	var textWidth = ctx.measureText(testo).width;  															//Ottengo la lunghezza del testo per la stampa
-	ctx.fillText(testo, (canvasWidth/2)-(textWidth/2), (canvasHeight/2)+(messageFontDim/2));				//Stampo
+	ctx.fillText(testo, (CANVASWIDTH/2)-(textWidth/2), (CANVASHEIGHT/2)+(MESSAGEFONTDIM/2));				//Stampo
 }
 
 //Gestione punteggio
 function Score() {
 	this.points = 0;
-	this.max = maxScore;
+	this.max = MAXSCORE;
 	this.add = function() {																					//Aumento il punteggio
 		this.points++;
 		if(this.points == this.max)	
-			message(winMessage);
+			message(WINMESSAGE);
 	}
 	this.print = function(){																				//Stampo il punteggio
-		ctx.fillStyle = messageColor;
-		ctx.font = scoreFontDim + "px " + font;   
+		ctx.fillStyle = MESSAGECOLOR;
+		ctx.FONT = SCOREFONTDIM + "px " + FONT;   
 		ctx.fillText("Score: " + this.points, 10, 10);
 	}
 }
